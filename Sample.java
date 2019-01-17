b4524eaee6552007540657ba810216c91649395f
Anna Gapuz <amgapuz@gmail.com>
2019-01-16 22:58:02 -0500

    Added more text to print out

--- Sample.java
+++ Sample.java
@@ -7,18 +7,22 @@ class Sample
 	{
 		// Creating an ArrayList with elements
 		// {1, 2, 3, 4}
-		ArrayList<Integer> arrL = new ArrayList<Integer>();
-		arrL.add(1);
-		arrL.add(2);
-		arrL.add(3);
-		arrL.add(4);
+		ArrayList<Integer> arrList = new ArrayList<Integer>();
+		arrList.add(1);
+		arrList.add(2);
+		arrList.add(3);
+		arrList.add(4);
+		arrList.add(5);
+		arrList.add(6);
 
 		// Using lambda expression to print all elements
-		// of arrL
-		arrL.forEach(n -> System.out.println(n));
+		// of arrList
+    System.out.println("Printing out the array");
+		arrList.forEach(n -> System.out.println(n));
 
 		// Using lambda expression to print even elements
-		// of arrL
-		arrL.forEach(n -> { if (n%2 == 0) System.out.println(n); });
+		// of arrList
+    System.out.println("Printing out even numbers");
+		arrList.forEach(n -> { if (n%2 == 0) System.out.println(n); });
 	}
 }
